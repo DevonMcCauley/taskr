32c9b2e2765ede37ccfd46cad37988c40d267ba5
Devon McCauley <54080684+DevonMcCauley@users.noreply.github.com>
2022-06-19 19:37:07 -0400

    Adjust footer

--- src/Layout/Footer/Footer.js
+++ src/Layout/Footer/Footer.js
@@ -1,7 +1,11 @@
 import classes from "./Footer.module.css";
 
 const Footer = () => {
-	return <div className={classes.footer__wrapper}>Footer</div>;
+	return (
+		<div className={classes.footer__wrapper}>
+			Taskr &copy; 2022 Devon McCauley
+		</div>
+	);
 };
 
 export default Footer;

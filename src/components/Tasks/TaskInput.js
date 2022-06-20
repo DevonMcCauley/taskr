735dd3223d772bab1a44c8b540d4c61c7f20a23c
Devon McCauley <54080684+DevonMcCauley@users.noreply.github.com>
2022-06-20 13:55:03 -0400

    Adding Tasks Fix

--- src/components/Tasks/TaskInput.js
+++ src/components/Tasks/TaskInput.js
@@ -4,11 +4,12 @@ import Button from "../UI/Button";
 import { useRef, useState } from "react";
 import Modal from "../UI/Modal/Modal";
 import { ToastContainer, toast } from "react-toastify";
+import { taskActions } from "../../app/taskSlice";
 
 // Returns an input field and a button to allow the user to add a task
 const TaskInput = () => {
 	const [modalIsOpen, setModalIsOpen] = useState(false);
-	// const dispatch = useDispatch();
+	const dispatch = useDispatch();
 	const taskNameRef = useRef("");
 	const taskDescRef = useRef("");
 	const notify = () =>
@@ -39,7 +40,7 @@ const TaskInput = () => {
 		};
 		taskNameRef.current.value = "";
 		taskDescRef.current.value = "";
-		// dispatch(addTask(task));
+		dispatch(taskActions.addTask(task));
 		notify();
 	};
 

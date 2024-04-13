a9985c0fd4c485169fe0b0e4dfa2e5900f9d7859
Devon McCauley <devmccauley@gmail.com>
2024-04-13 19:56:02 -0400

    Add basic authentication
    Implement authentication using Clerk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,41 @@
-import Image from "next/image";
+"use client";
+import { UserButton, useUser } from "@clerk/nextjs";
+import { useEffect } from "react";
 
 export default function Home() {
-	return <div>Home page</div>;
+	const { user } = useUser();
+
+	const apiURL = process.env.NEXT_PUBLIC_API_URL;
+
+	useEffect(() => {
+		user && createUserProfile();
+	}, [user]);
+
+	const createUserProfile = async () => {
+		// If the user is signed in, make a call to the backend to create/update the user
+		if (localStorage.getItem("isLogin") !== "true") {
+			if (user) {
+				const response = await fetch(`${apiURL}/users`, {
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json",
+					},
+					body: JSON.stringify({
+						firstName: user.firstName,
+						lastName: user.lastName,
+						email: user?.primaryEmailAddress?.emailAddress,
+						image: user.imageUrl,
+					}),
+				});
+			}
+			localStorage.setItem("isLogin", "true");
+		}
+	};
+
+	return (
+		<div>
+			Home Page
+			<UserButton />
+		</div>
+	);
 }
